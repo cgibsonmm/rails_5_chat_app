6f2e5b7eacc595ede6aaa99c659f26e2f9575f91
cgibsonmm <cgibsonmm@gmail.com>
2018-08-13 00:32:20 -0400

    work on side bar

--- app/helpers/chatrooms_helper.rb
+++ app/helpers/chatrooms_helper.rb
@@ -1,2 +1,9 @@
 module ChatroomsHelper
+  def in_chatroom?(chatroom)
+    in_room = false
+    chatroom.users.each do |user|
+      in_room = true if user.id == current_user.id
+    end
+    in_room
+  end
 end
